d0d63a5143ff00ae44c566e3f363a27b60e49e47
electroniccc <electroniccc@qq.com>
2025-03-09 11:27:19 +0800

    test

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -1,6 +1,15 @@
 fn main() {
     // TODO: Create an array called `a` with at least 100 elements in it.
     // let a = ???
+    let mut a = String::from("");
+    let mut i = 0;
+    loop {
+        if i > 100 {
+            break;
+        }
+        i += 1;
+        a += "a";
+    }
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
